2ca0ba9ca06a88a36e9cc838aba5dbc2bac921c0
Mahesh palaka <palakamahesh48325@gmail.com>
2023-02-18 23:13:25 +0530

    Update Length_longest_substring.cpp

--- Length_longest_substring.cpp
+++ Length_longest_substring.cpp
@@ -16,5 +16,24 @@ int main(){
         
 }
 
+//another method
+
+int n=S.size();
+        int ans=0;
+        int j=0,i=0;
+        unordered_map<char,int>mp;
+        while(j<n){
+            char r=s[j];
+            mp[r]++;
+            while(mp[r]>1){
+                 char l=s[i];
+                 mp[l]--;
+                 i++;
+             }
+            ans=max(ans, j-i+1);
+            j++;
+        }
+        return ans;
+
 
 //S = "geeksforgeeks"    ans=7;
